a43c399874e83f50c315a25ce01734da85aa1f1f
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-25 16:44:54 -0400

    Updates

---
+++ precache-manifest.137e36efd41ef7aa347b6bb952d8727e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09445ae84d81742c7e5155f186b22838",
+    "url": "/shopper-mapper/index.html"
+  },
+  {
+    "revision": "8da06598407ba0efc9cf",
+    "url": "/shopper-mapper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7598b3344f301363224",
+    "url": "/shopper-mapper/static/js/2.58f81cf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopper-mapper/static/js/2.58f81cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da06598407ba0efc9cf",
+    "url": "/shopper-mapper/static/js/main.b938f8ac.chunk.js"
+  },
+  {
+    "revision": "04a9ad81a4b17f8250e2",
+    "url": "/shopper-mapper/static/js/runtime-main.6c0cfe4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopper-mapper/static/media/logo.5d5d9eef.svg"
+  }
+]);
