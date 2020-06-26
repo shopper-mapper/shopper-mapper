9d05f83b401edd2fa11c5bb385de297c165c7a4d
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-26 01:46:02 -0400

    Test API's call, install axios dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      queryFromApi: [],
+      userInputlocation: '',
+    }
+  }
+
+
+  async componentDidMount() {
+    //  Pass lat/long from (Geocode Address API) here ↓ , in (Place Search API)
+    try {
+      // Retrieve query results + await for a promise to be resolved
+      const { data: { results } } = await axios({
+        url: "http://www.mapquestapi.com//search/v4/place",
+        method: "GET",
+        responseType: "JSON",
+        params: {
+          // Passing long, lat from  (Get Geocode Address API see below) to check the response
+          location: [-79.381713, 43.651893],
+          key: "RSBH9KbMvmkRzdRkD8Joil8TqbXW3HvB",
+          sort: "relevance",
+          // By default is museum, we'll interpolate user input instead
+          q: "museum",
+        }
+      })
+
+      console.log(results);
+
+      // Update state with the results data from an API call
+      this.setState({
+        queryFromApi: results,
+      })
+
+      // Handle error if promise is rejected
+    } catch (error) {
+      console.log(`Axios ruquest is failed ${error}`);
+    }
+
+    // Get Geocode Address ( longtitude, latitude) based on user input
+    try {
+      // Retrieve coordinates data (lat, long) + await for a promise to be resolved
+      const { data: { results: [{ locations: [{ latLng: { lat, lng } }] }] } } = await axios({
+        url: `http://www.mapquestapi.com/geocoding/v1/address`,
+        method: "GET",
+        responseType: "JSON",
+        params: {
+          key: "RSBH9KbMvmkRzdRkD8Joil8TqbXW3HvB",
+          // Default value, insted we'll interpolate user input
+          location: "Toronto,ON",
+        }
+      })
+
+
+      console.log(lat, lng);
+
+      // Handle error if promise is rejected
+    } catch (error) {
+      console.log(`Axios ruquest is failed ${error}`);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Shopper - Mapper</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
