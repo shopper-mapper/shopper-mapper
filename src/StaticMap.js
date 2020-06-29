d08474ffc71bc858a9ef8f3d42c99bff63e1a830
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-29 17:24:21 -0400

    Update styles, rename 'Form' component to 'Header'

--- src/StaticMap.js
+++ src/StaticMap.js
@@ -11,39 +11,39 @@ class StaticMap extends Component {
   }
 
 
-  async componentDidMount() {
-    try {
-          const mapData = await axios({
-            method: 'GET',
-            url: 'https://www.mapquestapi.com/staticmap/v5/map',
-            responseType: 'blob',
-            params: {
-              key: `tZVntk8rKYnj1VeUAi4cTD6mGHgEoP15`,
-              scalebar: 'true|bottom',
-            //   start: `Toronto, ON`,
-            //   end: `Windsor, ON`,
-              locations: `43.6532,-79.3832||42.3149,-83.0364`,
-            //   shape: `radius:10km|Toronto, ON`,
-              size: '600,600'
-            }
-          })
+  // async componentDidMount() {
+  //   try {
+  //         const mapData = await axios({
+  //           method: 'GET',
+  //           url: 'https://www.mapquestapi.com/staticmap/v5/map',
+  //           responseType: 'blob',
+  //           params: {
+  //             key: `tZVntk8rKYnj1VeUAi4cTD6mGHgEoP15`,
+  //             scalebar: 'true|bottom',
+  //           //   start: `Toronto, ON`,
+  //           //   end: `Windsor, ON`,
+  //             locations: `43.6532,-79.3832||42.3149,-83.0364`,
+  //           //   shape: `radius:10km|Toronto, ON`,
+  //             size: '600,600'
+  //           }
+  //         })
           
-          this.setState({
-            mapImageData : URL.createObjectURL(mapData.data),
-          })
-          //console.log (this.state.mapImageData)
+  //         this.setState({
+  //           mapImageData : URL.createObjectURL(mapData.data),
+  //         })
+  //         //console.log (this.state.mapImageData)
 
 
-        } catch (error) {
-            console.log(`Axios ruquest is failed ${error}`);
-        }
+  //       } catch (error) {
+  //           console.log(`Axios ruquest is failed ${error}`);
+  //       }
         
-  }
+  // }
 
   render() {
     return (
       <div>
-        <img src = {this.state.mapImageData}/>
+        <img src={this.props.mapImageData} alt=''/>
       </div>
     );
   }
