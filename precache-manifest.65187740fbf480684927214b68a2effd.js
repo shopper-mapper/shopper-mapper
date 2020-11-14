eb9de4e55cacd685410d6d41aadb33836cc8af0d
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-13 19:47:22 -0500

    Updates

---
+++ precache-manifest.65187740fbf480684927214b68a2effd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f1d2605d9c7947ce32bdb75cc67af2",
+    "url": "/shopper-mapper/index.html"
+  },
+  {
+    "revision": "6a95f68e78a6bfeaa122",
+    "url": "/shopper-mapper/static/css/main.b875df87.chunk.css"
+  },
+  {
+    "revision": "d1ea1cce4d25eb05015b",
+    "url": "/shopper-mapper/static/js/2.2f8f1d8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopper-mapper/static/js/2.2f8f1d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a95f68e78a6bfeaa122",
+    "url": "/shopper-mapper/static/js/main.439e4325.chunk.js"
+  },
+  {
+    "revision": "04a9ad81a4b17f8250e2",
+    "url": "/shopper-mapper/static/js/runtime-main.6c0cfe4a.js"
+  },
+  {
+    "revision": "e9e08731cee548ba891dc7c41df42671",
+    "url": "/shopper-mapper/static/media/map.e9e08731.jpg"
+  },
+  {
+    "revision": "87b087ac875191739c860b54cb6726ef",
+    "url": "/shopper-mapper/static/media/shopper-mapper-icon.87b087ac.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shopper-mapper/precache-manifest.2e91af3c5e5d5b9c35354ccc5b8246b7.js"
+  "/shopper-mapper/precache-manifest.65187740fbf480684927214b68a2effd.js"
 );
 
 self.addEventListener('message', (event) => {
